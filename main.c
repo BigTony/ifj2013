e11eca01bb5f22e7211246576bd826f4869c7e91
Antonin Seidl <tonyseidl.cz@gmail.com>
2013-10-09 01:38:33 +0200

    Pridany zdrojove soubory a nakonfigurovan Makefile

---
+++ main.c
@@ -0,0 +1,22 @@
+// Project interpret IFJ2013
+// Dev team:
+////////////////////////////////
+// Seidl Antonin  - xseidl01
+// Slunsky Tomas  - xsluns01
+// Skacel David   - xskace12
+// Skyva Petr     - xskyva02
+////////////////////////////////
+// main.c
+// Main program using other functions
+//
+
+
+#include <stdio.h>
+
+
+
+
+main(int argc,char** argv)
+{
+  return 0;
+}
