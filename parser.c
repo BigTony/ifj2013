c2d8e39b2487b92c876e017c0109a671355e4cef
jstc <jstc@b05-331b.(none)>
2013-10-11 13:32:41 +0200

    feat-add-new-files

---
+++ parser.c
@@ -0,0 +1,12 @@
+// Project interpret IFJ2013
+// Dev team:
+////////////////////////////////
+// Seidl Antonin  - xseidl01
+// Slunsky Tomas  - xsluns01
+// Skacel David   - xskace12
+// Skyva Petr     - xskyva02
+////////////////////////////////
+// interpret.c
+// 
+//
+
