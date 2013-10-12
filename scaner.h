53288cb3a59f2087b7e642b62470f811127371e9
jstc <jstc@b05-331b.(none)>
2013-10-12 16:28:53 +0200

    feat-tabulka-tokenu

--- scaner.h
+++ scaner.h
@@ -3,9 +3,57 @@
 */
 
 
+/**
+* Tabulka tokenu
+*/
+#define ID 0
+#define VARINT 1
+#define VARDOUBLE 2
+// Znaky
+#define ZAV_JEDN_L  10 // (
+#define ZAV_JEDN_P  11 // )
+#define ZAV_SLOZ_L  12 // {
+#define ZAV_SLOZ_P  13 // }
+#define STREDNIK	14 // ;
+// Operatory
+#define KRAT		22 // *
+#define DELENO		23 // /
+#define PLUS		20 // +
+#define MINUS		21 // - 
+#define TECKA		22 // .
+#define MENSI		23 // <
+#define VETSI		24 // >
+#define MENSI_ROVNO	25 // <=
+#define VETSI_ROVNO 26 // >=
+#define ROVNO 		27 // ===
+#define NEROVNO		28 // !==
+// Klicova slova
+#define IF			30 
+#define ELSE		31
+#define WHILE		32
+#define RETURN		33
+#define FUNCTION 	34
+// Konstanty
+#define NIL			40 // NULL
+
+// Funkce
 int getToken();
 
-// Klicova slova
-// TODO
-// Stavy pro automat
-// TODO
+
+
+
+
+/**
+* Struktury
+*/
+union Unie{
+    int varInt;
+    double varDouble;
+    char *varString;
+}tokenValue;
+
+typedef struct{
+	int id,
+	tokenValue value,
+}token;
+
