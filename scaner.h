adcf2b81adad400365e11f4c41b1fca414947f7d
Peter Skyva <petr.skyvaa@seznam.cz>
2013-11-27 22:03:52 +0100

    Update scaner.h
    nigga

--- scaner.h
+++ scaner.h
@@ -1,4 +1,4 @@
-git// Project interpret IFJ2013
+// Project interpret IFJ2013
 // Dev team:
 ////////////////////////////////
 // Seidl Antonin  - xseidl01
@@ -6,7 +6,7 @@ git// Project interpret IFJ2013
 // Skacel David   - xskace12
 // Skyva Petr     - xskyva02
 ////////////////////////////////
-// interpret.c
+// scaner.h
 // 
 //
 
@@ -17,24 +17,22 @@ git// Project interpret IFJ2013
 /**
 * Tabulka tokenu
 */
-
-#define ERROR -1 
-#define ID 0       
+#define FUNCTION_CALL 0
 #define VARINT 1
 #define VARDOUBLE 2
-#define VARSTRING 3 //"" 
+#define VARIABLE 3
 // Znaky
 #define ZAV_JEDN_L  10 // (
 #define ZAV_JEDN_P  11 // )
 #define ZAV_SLOZ_L  12 // {
 #define ZAV_SLOZ_P  13 // }
 #define STREDNIK	14 // ;
-#define UVOZ		15 // "
-#define APOST		16 // ' 
+#define CARKA		15 // ,
 // Operatory
-#define DELENO		19 // /
+#define KRAT		22 // *
+#define DELENO		23 // /
 #define PLUS		20 // +
-#define MINUS		21 // -
+#define MINUS		21 // - 
 #define TECKA		22 // .
 #define MENSI		23 // <
 #define VETSI		24 // >
@@ -42,6 +40,7 @@ git// Project interpret IFJ2013
 #define VETSI_ROVNO 26 // >=
 #define ROVNO 		27 // ===
 #define NEROVNO		28 // !==
+#define PRIRAZENI	29 // =
 // Klicova slova
 #define IF			30 
 #define ELSE		31
@@ -50,24 +49,13 @@ git// Project interpret IFJ2013
 #define FUNCTION 	34
 // Konstanty
 #define NIL			40 // NULL
-#define true        41
-#define false       42
-//-------------------
-#define EOF         45
-//-------------------
-//Unikove sekvence
-/*#define ESC_TAB     45 // \t
-#define ESC_N_L     46 // \n
-#define ESC_DOLAR   47 // \$
-#define ESC_ESC     48 // \\
-#define ESC_UVOZ    49 // \"   */
-
-//Alokace
-#define BUFF        100 //Alok ci realok vstupniho stringu
+// starty konce lol
+#define START 		50 // <?php
+#define KONEC		51 // eof
 
 // Funkce
-int getToken(FILE *fp);
-void skipSpace(char* *c, FILE *f);   //Nacte prvni znak za prazdnymi znaky.
+int getToken(Ttoken *token);
+
 /**
 * Struktury
 */
@@ -75,11 +63,11 @@ void skipSpace(char* *c, FILE *f);   //Nacte prvni znak za prazdnymi znaky.
 typedef union {
     int varInt;
     double varDouble;
-    char* *varString;
+    char *varString;
 }tokenValue;
 
 typedef struct{
 	int id;
 	tokenValue value;
-}token;
+}Ttoken;
 
