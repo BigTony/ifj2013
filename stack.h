fdd52de2f4bf0bbdc34c0505c32dbbda2bc87fe5
Peter Skyva <petr.skyvaa@seznam.cz>
2013-11-30 04:23:51 +0100

    vytvoreny struktury pro stack, enum pro hodnoty na zasobnik

--- stack.h
+++ stack.h
@@ -1,46 +1,56 @@
 typedef enum{
 	// hodnoty ktere jsou mozne ulozit na zasobnik
-	VALUE, // promena + konstanta
 	PLUS, // +
 	MINUS, // -
 	MULTI, // *
 	DIV, // /
+	KONK, // konkatenace .
 	EQ, // ===
 	NEQ, // !==
 	GTHE, // >=
 	LTHE, // <=
 	GTH, // >
 	LTH, // <
-	COM, // ,	
-	FUNC, // Funkce
 	BRACE_L, // (
 	BRACE_R, // )
-	KONK, // konkatenace .
 
-	// ridici symboly, decision
-	D_G, // >
-	D_L, // <
-	D_E, // =
-	D_B, // blank, prazdna cast tabulky
-	ERROR,
+	VALUE, // promena + konstanta
+	FUNC, // Funkce
+	COM, // ,	
+
+	
+	ERROR
 } TExpType;
 
-tabulka{		/* VALUE|PLUS|MINUS|MULTI|DIV|EQ|NEQ|GTHE|LTHE|GTH|LTH|COM|FUNC|BRACER_L|BRACE_R|KONK|END*/
-	/* VALUE*/		{D_B,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_B,D_B,D_G,D_G,D_G},
-	/* PLUS*/		  {D_L,D_G,D_G,D_L,D_L,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_L,D_L,D_G,D_G,D_G},
-	/* MINUS*/		{D_L,D_G,D_G,D_L,D_L,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_L,D_L,D_G,D_G,D_G},
-	/* MULTI*/		{D_L,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_L,D_L,D_G,D_G,D_G},
-	/* DIV */	  	{D_L,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_L,D_L,D_G,D_G,D_G},
-	/* EQ */		  {D_L,D_L,D_L,D_L,D_L,D_G,D_G,D_L,D_L,D_L,D_L,D_G,D_L,D_L,D_G,D_L,D_G},
-	/* NEQ */		  {D_L,D_L,D_L,D_L,D_L,D_G,D_G,D_L,D_L,D_L,D_L,D_G,D_L,D_L,D_G,D_L,D_G},
-	/* GTHE */		{},
-	/* LTHE */		{},
-	/* GTH */		  {},
-	/* LTH */		  {},
-	/* COM */		  {D_L,D_L,D_L,D_L,D_L,D_L,D_L,D_L,D_L,D_L,D_L,D_E,D_L,D_L,D_E,D_L,D_B},
-	/* FUNC */		{},
-	/* BRACE_L */	{},
-	/* BRACE_R */	{},
-	/* KONK */		{D_L,D_G,D_G,D_L,D_L,D_G,D_G,D_G,D_G,D_G,D_G,D_G,D_L,D_L,D_G,D_G,D_G},
-	/* END */   	{D_L,D_L,D_L,D_L,D_L,D_L,D_L,D_L,D_L,D_L,D_L,D_B,D_L,D_L,D_B,D_L,D_B}
-}
+#define MAX_INDEX 17
+// ridici symboly
+#define G 0 // >
+#define L 1 // <
+#define E 2 // =
+#define B 3 // blank, prazdna cast tabulky
+
+
+// struktury pro zasobnik
+typedef struct TSItem{
+	TExpType item;
+	struct TSItem *ptrNext;
+} *TSItemPtr;
+
+typedef struct {
+	TSItemPtr top;
+} TStack;
+
+// init zasobniku
+void SInit(TStack *stack);
+// vloz prvek na zasobnik
+void SPush(TStack *stack,TExpType item);
+// odstran prvek z vrcholu zasobniku
+void SPop(TStack *stack);
+// nacteni z vrcholu
+void STop(TStack *stack, TExpType *item);
+// nacteni z vrcholu a odstraneni prvku
+void STopPop(TStack *stack, TExpType *item);
+// bool prazdny zasobnik
+int SEmpty(TStack *stack);
+
+
