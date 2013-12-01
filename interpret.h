c4cfff0eaec201215dd4bb5f3265d4e447a4face
Tomáš Slunsky <t.slunsky@gmail.com>
2013-12-01 23:18:38 +0100

    knihovny

---
+++ interpret.h
@@ -0,0 +1,19 @@
+// Project interpret IFJ2013
+// Dev team:
+////////////////////////////////
+// Seidl Antonin - xseidl01
+// Slunsky Tomas - xsluns01
+// Skacel David - xskace12
+// Skyva Petr - xskyva02
+////////////////////////////////
+// interpret.c
+//
+//
+
+#include <string.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "list.h"
+#include "stack.h"
