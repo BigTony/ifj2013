2ce2c35e613ee765609a2a910073b65eab3f7de9
Peter Skyva <petr.skyvaa@seznam.cz>
2013-12-01 04:28:05 +0100

    Update stack.h

--- stack.h
+++ stack.h
@@ -13,21 +13,26 @@ typedef enum{
 	LTH, // <
 	BRACE_L, // (
 	BRACE_R, // )
-
 	VALUE, // promena + konstanta
 	FUNC, // Funkce
 	COM, // ,	
+	END, // $
 
-	
+	// nonterminal
+	NONTERM, // pravidlo r
+	G,
+	L,
+	E,
+	B,	
 	ERROR
 } TExpType;
 
 #define MAX_INDEX 17
-// ridici symboly
-#define G 0 // >
-#define L 1 // <
-#define E 2 // =
-#define B 3 // blank, prazdna cast tabulky
+
+// #define G 0 // >
+// #define L 1 // <
+// #define E 2 // =
+// #define B 3 // blank, prazdna cast tabulky
 
 
 // struktury pro zasobnik
@@ -47,10 +52,12 @@ void SPush(TStack *stack,TExpType item);
 // odstran prvek z vrcholu zasobniku
 void SPop(TStack *stack);
 // nacteni z vrcholu
-void STop(TStack *stack, TExpType *item);
-// nacteni z vrcholu a odstraneni prvku
-void STopPop(TStack *stack, TExpType *item);
+TExpType STop(TStack *stack);
 // bool prazdny zasobnik
 int SEmpty(TStack *stack);
+// odstran vsechny prvky
+void SPopAll(TStack *stack);
+
+
 
 
