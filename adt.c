73ddf3aa7ac225201f57c9d52cab17b016767c9a
jstc <jstc@b05-331b.(none)>
2013-12-03 21:05:39 +0100

    add adt

---
+++ adt.c
@@ -0,0 +1,10 @@
+// Project interpret IFJ2013
+// Dev team:
+////////////////////////////////
+// Seidl Antonin  - xseidl01
+// Slunsky Tomas  - xsluns01
+// Skacel David   - xskace12
+// Skyva Petr     - xskyva02
+////////////////////////////////
+// adt.c
+//
