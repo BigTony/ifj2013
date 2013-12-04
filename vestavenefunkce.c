eb3b99e86959ad3b83cb443b63d17792be0ba1bc
JoystiC <petr.skyvaa@seznam.cz>
2013-12-04 20:36:04 +0100

    update prevod funkce

--- vestavenefunkce.c
+++ vestavenefunkce.c
@@ -1,15 +1,38 @@
+#include <limits.h>
+#include <ctype.h>
+
 // pretypovani
 void tovarint(item *item){
 	switch(item->type){
 		case BOOL:
-			item->type = BOOL;
+			item->type = VARINT;
 			break;
 		case VARDOUBLE:
-			item->type = VARDOUBLE;
+			item->data = (int)item->data;
+			item->type = VARINT;
 			break;
 		case STRING:
-
-			item->type = STRING;
+			int i = 0;
+			int cislo = 0;
+			while ((item->data[i]!='\0')){
+				if((isspace(item->data[i]))&&(blank)&&(cislo==0)) {
+					continue;
+				}
+  				if (isdigit(item->data[i]) && (cislo<=INT_MAX)){
+  					cislo=cislo*10+(item->data[i]-'0');
+  					i++;
+  				}else{
+  					break;
+  				}
+  			}
+  			free(item->data);
+  			if(blank == 0)
+  				item->data = cislo;
+  			else
+  				item->data = 0;
+			item->type = VARINT;
+			break;
+		case VARINT:
 			break;
 		default:
 			print_error(E_INT,"chyba pri pretypovani");
@@ -23,23 +46,27 @@ void tobool(item *item){
 			if(item->data != 0){
 				item->data = 1;
 			}
-			item->type = VARINT;
+			item->type = BOOL;
 			break;
 		case VARDOUBLE:
-			if(item->data == 0){
-				item->data = 0.0;
+			if(item->data == 0.0){
+				item->data = 0;
 			}else{
-				item->data = 1.0;
+				item->data = 1;
 			}
-			item->type = VARDOUBLE;
+			item->type = BOOL;
 			break;
 		case STRING:
-			if(item->data == 0){
-				item->data = "";
+			if(strcmp (item->data,"") == 0){
+				free(item->data);
+				item->data = 0;
 			}else{
-				item->data = "1";
+				free(item->data);
+				item->data = 1;
 			}
-			item->type = STRING;
+			item->type = BOOL;
+			break;
+		case BOOL:
 			break;
 		default:
 			print_error(E_INT,"chyba pri pretypovani");
@@ -50,19 +77,52 @@ void tobool(item *item){
 void todouble(item *item){
 	switch(item->type){
 		case VARINT:
-			item->data = (int)item->data;
-			item->type = VARINT;
+			item->type = VARDOUBLE;
 			break;
 		case STRING:
-
+			int i = 0;
+			int c = 0;
+			int cislo = 0;
+			int desetiny_cislo = 0;
+			int carka = 0;
+			while ((item->data[i]!='\0')){
+				if((isspace(item->data[i]))&&(blank)&&(cislo==0)&&(desetiny_cislo==0)) {
+					continue;
+				}
+  				if (isdigit(item->data[i]) && (cislo<=INT_MAX)&&(desetiny_cislo==0)){
+  					if(carka == 1){
+  						desetiny_cislo=desetiny_cislo+(item->data[i]-'0')/10;	
+  					}else{
+  						cislo=cislo*10+(item->data[i]-'0');
+  					}
+  					i++;
+  				}else if((item->data[i] == '.')){
+  					if(carka == 0){
+  						carka = 1;
+  					}else{
+  						break;
+  					}
+  					
+  				}else{
+  					break;
+  				}
+  			}
+  			free(item->data);
+  			if(blank == 0)
+  				item->data = cislo+desetiny_cislo;
+  			else
+  				item->data = 0.0;
+			item->type = VARDOUBLE;
 			break;
 		case BOOL:
-			if(item->data == 0.0){
-				item->data = 0;
+			if(item->data == 0){
+				item->data = 0.0;
 			}else{
-				item->data = 1;
+				item->data = 1.0;
 			}
-			item->type = BOOL;
+			item->type = VARDOUBLE;
+			break;
+		case VARDOUBLE:
 			break;
 		default:
 			print_error(E_INT,"chyba pri pretypovani");
@@ -70,25 +130,39 @@ void todouble(item *item){
 	}
 }
 
+int get_int_len (int value){
+  int l=1;
+  while(value>9){ 
+  	l++; value/=10; 
+  }
+  return l;
+}
+
 void tostring(item *item){
 	switch(item->type){
 		case VARINT:
-
+			int delka = get_int_len(item->data);
+			char *vysledek[delka+1];
+			itoa(item->data,vysledek,10);
+			item->data = vysledek;
 			item->type = VARINT;
 			break;
 		case VARDOUBLE:
-
+			// delka double cisla?
+			char *vysledek[100];
+			sprintf(vysledek, "%g", item->data);
+			item->data = vysledek;
 			item->type = VARDOUBLE;
 			break;
 		case BOOL:
-			if(strcmp (item->data,"") == 0){
-				free(item->data);
-				item->data = 0;
+			if(item->data == 0){
+				item->data = "";
 			}else{
-				free(item->data);
-				item->data = 1;
+				item->data = "1";
 			}
-			item->type = BOOL;
+			item->type = STRING;	
+			break;
+		case STRING:
 			break;
 		default:
 			print_error(E_INT,"chyba pri pretypovani");
@@ -138,6 +212,15 @@ void boolval(item *item){
 	}
 }
 
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(){
+	double a=2.132;
+	char arr[23];
+	sprintf(arr, "%g", a);
+	printf("%s\n",arr);
 	return 0;
 }
