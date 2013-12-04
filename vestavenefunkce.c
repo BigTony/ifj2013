9b849307fc46d2341d76e8b1653da37c6a52a74c
JoystiC <petr.skyvaa@seznam.cz>
2013-12-04 17:10:56 +0100

    update

---
+++ vestavenefunkce.c
@@ -0,0 +1,24 @@
+#include "ial.h"
+
+void boolval(item *item){
+	switch(item->type){
+		case VARINT:
+			item->type = VARINT;
+			break;
+		case VARDOUBLE:
+			break;
+		case STRING:
+			break;
+		case VARBOOL:
+			break;
+		case NIL:
+			break;
+		default:
+			print_error(E_SYN,"token nelze prevest");
+			break;
+	}
+}
+
+int main(){
+	return 0;
+}
