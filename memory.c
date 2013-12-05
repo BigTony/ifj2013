2743a71d5a6dbed33f48c78727b75d071464d80d
JoystiC <petr.skyvaa@seznam.cz>
2013-12-05 15:04:21 +0100

    memory soubory

---
+++ memory.c
@@ -0,0 +1,39 @@
+#include "memory.h"
+
+/**Inicializace globalni tabulky pointru
+ * @param tPointers: ukazatel na globalni tabuku pointru   
+ */  
+void init_global(tPointers *ptrs)
+{
+	if (NULL == (ptrs=malloc(sizeof(tPointers))))
+	{
+		print_error(E_INTERN,"Chyba alokace pameti: init_global");
+	}
+	ptrs->counter=allocString();
+	//nastavit na $$\0
+	ptrs->token=NULL;
+	ptrs->main_symobol_tbl=NULL
+	ptrs->function_stack=NULL;
+	ptrs->list_instr_list=NULL;
+	ptrs->FILE=NULL;
+}
+/**Dealokace globalni tabulky pointru
+ *@param tPointers: ukazatel na globalni tabuku pointru   
+ */   
+void destr_global(tPointers *ptrs)
+{
+	
+	destr_tToken(ptrs->token);
+	destr_tHashTbl(ptrs->main_symobol_tbl);
+	destr_tHashTblStack(ptrs->function_stack);
+	destr_tListIns(ptrs->list_instr_list);
+	if (FILE!=NULL)
+	{
+		fclose(source);
+	}
+	if (counter!=NULL)
+	{
+		free(counter);
+	}
+	free(ptrs);
+}
---
+++ memory.h
@@ -0,0 +1,27 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+#include "ilist.h"
+#include "ial.h"
+#include "scaner.h"
+#include "adt.h"
+// struktura pro dealokaci a errory
+// vsechno co se naalokuje a vyuziva se mezi jednotlivimi moduly se ulozi sem
+typedef struct tPointers{
+	Ttoken *token;	//predavany token
+	struct tHashTbl *main_symobol_tbl;	//hlavni tabulka symbolu
+	struct tHashTblStack *function_stack;	//zasobnik tabulek symbolu pro funkce
+	struct TList *list_instr;	//Ukazatel List main instrukci 
+	struct TList *act_list_inst; //Ukazatel na aktulani instruction list
+	FILE *source;	//ukazatel na v vstupni soubor
+	char * counter;
+}tPointers;
+
+extern tPointers *ptrs;
+
+// Funkce pro alokaci, inicializaci a dealokaci globalnich promenych
+void init_global(tPointers *ptrs);
+void dealloc_global(tPointers *ptrs);
+// Pole pro alokacni funkce
+typedef void (*ptrDealoc) (tPointers *ptrs);
+// Pole pro dealokacni funkce 
+#endif
