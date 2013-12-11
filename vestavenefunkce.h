ca4e4d9ae2d7dbbe26b117feef8cf628ef6d70bc
JoystiC <petr.skyvaa@seznam.cz>
2013-12-11 16:27:43 +0100

    ff

--- vestavenefunkce.h
+++ vestavenefunkce.h
@@ -9,13 +9,13 @@
 //vraci -1 nenajde-li
 int get_substring(char* text,char* word);
 
-void vs_boolval(tHashTbl *tab,item *item);
-void vs_doubleval(tHashTbl *tab,item *item);
-void vs_intval(tHashTbl *tab,item *item);
-void vs_strval(tHashTbl *tab,item *item);
-void vs_get_string(tHashTbl *tab,item *item);
-void vs_put_string(tHashTbl *tab,item *item);
-void vs_strlen(tHashTbl *tab,item *item);
-void vs_get_substring(tHashTbl *tab,item *item);
-void vs_find_string(tHashTbl *tab,item *item);
-void vs_sort_string(tHashTbl *tab,item *item);
+void vs_boolval(tHashTbl *tab,tHashTbl *NavrTab);
+void vs_doubleval(tHashTbl *tab,tHashTbl *NavrTab);
+void vs_intval(tHashTbl *tab,tHashTbl *NavrTab);
+void vs_strval(tHashTbl *tab,tHashTbl *NavrTab);
+void vs_get_string(tHashTbl *tab,tHashTbl *NavrTab);
+void vs_put_string(tHashTbl *tab,tHashTbl *NavrTab);
+void vs_strlen(tHashTbl *tab,tHashTbl *NavrTab);
+void vs_get_substring(tHashTbl *tab,tHashTbl *NavrTab);
+void vs_find_string(tHashTbl *tab,tHashTbl *NavrTab);
+void vs_sort_string(tHashTbl *tab,tHashTbl *NavrTab);
